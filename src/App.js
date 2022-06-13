6dea3a4d3f6f77b70e751690728234483a79dfd1
RayMiller7 <raymond@doohit.fr>
2022-06-14 00:22:06 +0200

    Create sidebar and main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./App.css"
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
 
 function App() {
   return (
     <div className='main-container'>
-
+        <Sidebar />
+        <Main />
     </div>
   )
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./main.css"
+
+function Main() {
+  return (
+    <section className='main'></section>
+  )
+}
+
+export default Main
