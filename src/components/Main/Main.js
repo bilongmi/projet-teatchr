411a467384989faf1e9f4f139fe5d7ee902b201c
RayMiller7 <raymond@doohit.fr>
2022-06-14 03:22:10 +0200

    Starting main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./main.css"
+import Loupe from "../../images/loupe.png"
 
 function Main() {
-  return (
-    <section className='main'></section>
-  )
+    return (
+        <section className='main'>
+            <header>
+                <h1 className='main-title'>En quelle matière avez vous besoin d'aide ?</h1>
+                <div className='searchbar'>
+                  <input type="text" placeholder='SVT, Maths, Physiques, ...'/>
+                  <img src={Loupe} />
+                </div>
+            </header>
+        </section>
+    )
 }
 
 export default Main
